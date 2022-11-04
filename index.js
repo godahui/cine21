c8ac92fb11442ef7755c925f2e55bf4df645c23b
Godahui <joyjoy0676@gmail.com>
2022-11-04 14:19:04 +0900

    슬라이드 이벤트 추가

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// 헤더
 $(document).ready(function () {
   // 검색창 toggle
   $(".search-btn").click(function () {
@@ -20,4 +21,63 @@ $(document).ready(function () {
     $(".nav").removeClass("actived");
     $(this).addClass("actived");
   });
+});
+
+// 메인 슬라이드
+//자동재생 함수선언
+let currentIndex = 0;
+
+function autoPlay() {
+  intervalKey = setInterval(function () {
+    if (currentIndex < 2) {
+      currentIndex++;
+    } else {
+      currentIndex = 0;
+    }
+
+    $(".item").eq(currentIndex).siblings().fadeOut();
+    $(".item").eq(currentIndex).fadeIn();
+    $(".step").removeClass("active-step");
+    $(".step").eq(currentIndex).addClass("active-step");
+  }, 3000);
+}
+
+//일시정지 함수선언
+function autoPause() {
+  clearInterval(intervalKey);
+  $(".pause-btn").addClass("on").prev().removeClass("on");
+}
+
+//플레이 버튼
+$(".play-btn").on("click", function (evt) {
+  evt.preventDefault();
+
+  $(this).addClass("on").next().removeClass("on");
+  autoPlay();
+});
+
+//일시정지 이벤트
+$(".pause-btn").on("click", function (evt) {
+  evt.preventDefault();
+
+  autoPause();
+});
+
+//페이지 로드시 자동재생
+$(window).on("load", function () {
+  autoPlay();
+});
+
+// 기사 슬라이드
+let newsIndex = 0;
+
+$navItem = $(".news-nav-item");
+$newsList = $(".news-list");
+
+$navItem.click(function () {
+  $navItem.removeClass("actived");
+  $(this).addClass("actived");
+  $index = $(this).data("index");
+  console.log($index);
+  $newsList.css("margin-left", "-" + `${100 * $index}` + "%");
 });
